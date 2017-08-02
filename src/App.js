cdd46283850cfb7f30063525fbc62a0e814a43de
tunna <tunna@edcast.com>
2017-08-02 12:48:50 +0530

    Event handling in It

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React from 'react'
 
 class App extends React.Component
 {
+  constructor()
+  {
+    super();
+    this.state={ txt : "this is state text ." , count : 100}
 
-  render()
-  { let t2 = this.props.txt2 
-  //1. use this.props to get value
-    return (
-      <div>
-      <h1 >Props text1= {this.props.txt1} </h1>
-      <h2>Props text2= {t2}</h2>
-      <h1> Props Default = {this.props.txt3}</h1>
-      </div>  )
   }
+
+  update(e)
+  {
+    this.setState( {txt: e.target.value } )
+  }
+  render()
+  {
+    return(
+      <div>
+        <input type="text" onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt} -> count = {this.state.count}</h1>
+      </div>
+      )
+  }
+
 }
 
-App.propTypes= {
-
-txt1: React.PropTypes.string , 
-//cat: React.ProTypes.number.isRequired
-}
-
-App.defaultProps={
-
-  txt3: "This is 3rd Text..."
-}
 
 export default App
