f76e3214faead8e79ac20ebd22e07fe45d577cbd
tunna <tunna@edcast.com>
2017-08-02 12:38:22 +0530

    Default prop value

--- src/App.js
+++ src/App.js
@@ -10,10 +10,20 @@ class App extends React.Component
       <div>
       <h1 >Props text1= {this.props.txt1} </h1>
       <h2>Props text2= {t2}</h2>
+      <h1> Props Default = {this.props.txt3}</h1>
       </div>  )
   }
 }
 
+App.propTypes= {
 
+txt1: React.PropTypes.string , 
+//cat: React.ProTypes.number.isRequired
+}
+
+App.defaultProps={
+
+  txt3: "This is 3rd Text..."
+}
 
 export default App
